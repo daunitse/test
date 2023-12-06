6445c27c97f9bcecfe428809ef7942aa6a9c5dda
daunitse <vladimir.nalimovv@gmail.com>
2023-12-07 00:13:34 +0300

    test

---
+++ 1.13.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var a, g int
+	_, err := fmt.Scanln(&a)
+	if err != nil {
+		panic(err)
+	}
+	for i := 1; i < a; i++ {
+		g = math.Pow(float64(i), float64(2))
+	}
+}
